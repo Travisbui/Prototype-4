13d76881b0c4b62d65dec64512ab4c53520d706c
Travisbui <122054086+Travisbui@users.noreply.github.com>
2023-04-06 11:22:46 -0700

    Challenge 1.7

--- Prototype 4/Assets/Challenge 4/Scripts/SpawnManagerX.cs
+++ Prototype 4/Assets/Challenge 4/Scripts/SpawnManagerX.cs
@@ -25,7 +25,6 @@ public class SpawnManagerX : MonoBehaviour
         if (enemyCount == 0)
         {
             SpawnEnemyWave(waveCount);
-            waveCount++;
         }
 
     }
@@ -50,7 +49,7 @@ public class SpawnManagerX : MonoBehaviour
         }
 
         // Spawn number of enemy balls based on wave number
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < enemiesToSpawn; i++)
         {
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
         }
